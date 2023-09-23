4099210388eb4784e576066fcc1c118ec1d3bba4
RBastos36 <you@example.comr.bastos@ua.pt>
2023-09-23 11:01:57 +0100

    Smoothing Images Tutorial Finished

---
+++ Tutorial_SmoothingImages.py
@@ -0,0 +1,41 @@
+
+# 2D Convolution
+
+# import numpy as np
+# import cv2 as cv
+# from matplotlib import pyplot as plot
+
+# img = cv.imread("/home/rantonio/Desktop/messi.jpg")
+# assert img is not None, "file could not be read, check with os.path.exists()"
+
+# kernel = np.ones((5, 5,), np.float32) / 25
+# dst = cv.filter2D(img, -1, kernel)
+
+# plot.subplot(121), plot.imshow(img), plot.title("Original")
+# plot.xticks([]), plot.yticks([])
+# plot.subplot(122), plot.imshow(dst), plot.title("Averaging")
+# plot.xticks([]), plot.yticks([])
+# plot.show()
+
+# --------------------------------------------------------
+
+# Image Blurring - Averaging
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plot
+
+img = cv.imread('/home/rantonio/Desktop/messi.jpg')
+assert img is not None, "file could not be read, check with os.path.exists()"
+
+# blur = cv.blur(img,(5, 5))
+# blur = cv.boxFilter(src=img, ksize=(5, 5), ddepth=-1, normalize=False)
+# blur = cv.GaussianBlur(img, (5, 5), 0)
+# blur = cv.medianBlur(img,5)
+blur = cv.bilateralFilter(img, 9, 75, 75)
+
+plot.subplot(121),plot.imshow(img),plot.title('Original')
+plot.xticks([]), plot.yticks([])
+plot.subplot(122),plot.imshow(blur),plot.title('Blurred')
+plot.xticks([]), plot.yticks([])
+plot.show()
